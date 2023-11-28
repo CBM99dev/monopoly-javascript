444558cb7459bc65a8d0097819d63d306c3c5aa6
CBM99dev <connor_email@yahoo.com>
2023-11-27 20:35:40 -0500

    passed go
    and added money

--- monopoly/monopoly.js
+++ monopoly/monopoly.js
@@ -18,19 +18,69 @@ var boardSpaces = [
         name: 'Baltic Avenue',
         leftPos: 660,
         topPos: 925
-    }
+    },
+    {
+        name: 'Income Tax',
+        leftPos: 580,
+        topPos: 925
+    },
+    {
+        name: 'Reading Railroad',
+        leftPos: 500,
+        topPos: 925
+    },
+    {
+        name: 'Short Line Railroad',
+        leftPos: 925,
+        topPos: 500
+    },
+    {
+        name: 'Chance 3',
+        leftPos: 925,
+        topPos: 580
+    },
+    {
+        name: 'Park Place',
+        leftPos: 925,
+        topPos: 660
+    },
+    {
+        name: 'Luxury Tax',
+        leftPos: 925,
+        topPos: 740
+    },
+    {
+        name: 'Boardwalk',
+        leftPos: 925,
+        topPos: 820
+    },
 ]
 
 var myPlayer = {
     spaceNumber: 0,
+    money: 1000,
     leftPos: 925,
     topPos: 925
 }
 
+function logPlayerData(){
+    var dN = document.getElementById("debug notes");
+    dN.innerHTML = myPlayer.money;
+}
+
+
 function movePiece() {
     var myPiece = document.getElementById("piece");
-    myPlayer.spaceNumber += 1;
-    myPiece.style.left = boardSpaces[myPlayer.spaceNumber].leftPos;
+    if(boardSpaces[myPlayer.spaceNumber].name == 'Boardwalk') {
+        myPlayer.spaceNumber = 0;
+        myPlayer.money += 200;
+    } else {
+        myPlayer.spaceNumber += 1;
+    }
     
-
+    
+    
+    myPiece.style.left = boardSpaces[myPlayer.spaceNumber].leftPos;
+    myPiece.style.top = boardSpaces [myPlayer.spaceNumber].topPos;
+    logPlayerData();
 }
