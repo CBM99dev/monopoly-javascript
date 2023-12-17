463647f75880b999130092f632d482c6dbb5e099
Lester Martin <lester.martin@gmail.com>
2023-12-17 18:43:52 -0500

    new board layout
    rearranged the board to be more flat to fit into a browser better.  added a few more spaces on the board and adjusted all the x:y posiitons.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # monopoly-javascript
+
+Just a little father & son programming activity for fun and learning.
+
+DEFINITELY a work-in-progress, but we are always up for some help.  :)
+
+AFTER you clone (or download) this project, you can start the game with your browser by opening this file directly.
+
+~/monopoly/index.html
--- monopoly/monopoly.js
+++ monopoly/monopoly.js
@@ -1,71 +1,108 @@
 var boardSpaces = [
     {
+    	propID: 0,
         name: 'GO',
-        leftPos: 925,
-        topPos: 925
+        leftPos: 845,
+        topPos: 613
     },
     {
+    	propID: 1,
         name: 'Mediterranean Avenue',
-        leftPos: 825,
-        topPos: 925
+        leftPos: 738,
+        topPos: 637
     },
     {
+    	propID: 2,
         name: 'Community Chest 1',
-        leftPos: 740,
-        topPos: 925
+        leftPos: 665,
+        topPos: 637
     },
     {
+    	propID: 3,
         name: 'Baltic Avenue',
-        leftPos: 660,
-        topPos: 925
+        leftPos: 592,
+        topPos: 637
     },
     {
+    	propID: 4,
         name: 'Income Tax',
-        leftPos: 580,
-        topPos: 925
+        leftPos: 516,
+        topPos: 637
     },
     {
+    	propID: 5,
         name: 'Reading Railroad',
-        leftPos: 500,
-        topPos: 925
+        leftPos: 442,
+        topPos: 637
     },
     {
+    	propID: 6,
+        name: 'Oriental Avenue',
+        leftPos: 368,
+        topPos: 637
+    },
+    {
+    	propID: 7,
+        name: 'Chance 1',
+        leftPos: 294,
+        topPos: 637
+    },
+    {
+    	propID: 8,
+        name: 'Vermont Avenue',
+        leftPos: 220,
+        topPos: 637
+    },
+    {
+    	propID: 9,
+        name: 'Connecticut Avenue',
+        leftPos: 146,
+        topPos: 637
+    },
+    {
+    	propID: 35,
         name: 'Short Line Railroad',
-        leftPos: 925,
-        topPos: 500
+        leftPos: 812,
+        topPos: 283
     },
     {
+    	propID: 36,
         name: 'Chance 3',
-        leftPos: 925,
-        topPos: 580
+        leftPos: 812,
+        topPos: 349
     },
     {
+    	propID: 37,
         name: 'Park Place',
-        leftPos: 925,
-        topPos: 660
+        leftPos: 812,
+        topPos: 414
     },
     {
+    	propID: 38,
         name: 'Luxury Tax',
-        leftPos: 925,
-        topPos: 740
+        leftPos: 812,
+        topPos: 480
     },
     {
+    	propID: 39,
         name: 'Boardwalk',
-        leftPos: 925,
-        topPos: 820
-    },
+        leftPos: 812,
+        topPos: 546
+    }
 ]
 
 var myPlayer = {
     spaceNumber: 0,
     money: 1000,
-    leftPos: 925,
-    topPos: 925
+    leftPos: 845,
+    topPos: 613
 }
 
 function logPlayerData(){
     var dN = document.getElementById("debug notes");
-    dN.innerHTML = myPlayer.money;
+    dN.innerHTML = '<hr>' +
+                   'Player details: <br>' +
+                   '- Cash = ' + myPlayer.money + 'space # = ' + mPlayer.spaceNumber
 }
 
 
